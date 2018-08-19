dbedd6240fe013ba13c82cfa8b5a00eae3388cc2
Xavi <xavierdcruz0@gmail.com>
2018-08-19 17:17:40 +0100

    modified data downloader script

--- download_mnist_data.sh
+++ download_mnist_data.sh
@@ -1,5 +1,7 @@
 # First navigate to the folder to download data to:
-cd ai_workshop/mnist_exercise/exercises/images
+cd ai_workshop/mnist_exercise/exercises
+mkdir images
+cd images
 
 # And download the 4 archives:
 curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
